ec1dfda4a2656ea11e6dc99a9549297ce93db093
Aaron Nguyen <aaronnguyen@wifi-10-40-65-164.wifi.berkeley.edu>
2024-10-10 16:16:51 -0700

    ./components/Aaron.tsx

--- defenders-assignment/src/app/components/Aaron.tsx
+++ defenders-assignment/src/app/components/Aaron.tsx
@@ -1,5 +1,16 @@
 export default function Aaron() {
     return (
-        <div className=""></div>
+        <div>
+             <span className="text-red-500"> Hello this</span>
+             <span className="text-orange-500"> is Aaron </span>
+            <span className="text-yellow-500"> and I finally </span>
+            <span className="text-green-500"> did it! </span>
+            <span className="text-blue-500"> I am here </span>
+            <span className="text-purple-500"> in Moffit! </span>
+           
+            
+            </div>
+        
+        
     );
 }
